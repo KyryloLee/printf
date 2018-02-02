c7c8b3496b7fd016dfb5e76887ed71373dd10e64
Kirill LEE <klee@e1r9p8.unit.ua>
2018-02-02 18:01:48 +0200

    13

--- srcs/type_1.c
+++ srcs/type_1.c
@@ -38,13 +38,16 @@ void	f_type_c(char **res, va_list ap)
 void	f_type_bc(char **res, va_list ap)
 {
 	char	c[2];
+	int		i;
 
 	c[1] = '\0';
+	i = va_arg(ap, int);
+	i = !i ? 1 : i;
 	if (MB_CUR_MAX > 1)
-		f_wchar_t(res, va_arg(ap, int));
+		f_wchar_t(res, i);
 	else
 	{
-		c[0] = (char)va_arg(ap, int);
+		c[0] = (char)i;
 		f_join_free(res, c);
 	}
 }
